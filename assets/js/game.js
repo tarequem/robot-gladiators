6ba10b408b97fa1859f579a16efd003cccbe8d20
tarequem <moatartareque@gmail.com>
2022-05-21 15:03:54 -0400

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,20 @@
-var playerName = window.prompt("what is your robot's name?");
+// pseudocode: Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-var fight = function () {
+var fight = function (enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -69,7 +72,22 @@ var fight = function () {
 };// end of fight function
 
 // run fight function to start game
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /* everything below is for learning and practicing purposes from the module
@@ -114,5 +132,35 @@ if (confirmSkip === true) {
 if (confirmSkip) {
   // do something
 }
+
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames[3]);
+console.log(enemyNames.length);
+
+for(var i = 0; i < 3; i++) {
+    console.log("apple", i)
+}
+
+var playerName = window.prompt("what is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
+console.log(playerName, playerAttack, playerHealth);
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+var enemyHealth = 50;
+var enemyAttack = 12;
+
 
 */
