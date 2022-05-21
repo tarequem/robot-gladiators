de9d5a01f7b7a63a71a1d5228ffdfdea89219dc1
tarequem <moatartareque@gmail.com>
2022-05-20 20:01:45 -0400

    added naming robot function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+//window.alert(playerName);
+console.log(playerName);
+console.log("This logs a strign, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("The fight as begun!");
+}
+
+//fight();
